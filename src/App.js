0bb67d8e209263fe1dbab966f8dbe1a444cda59e
Heizenburger <143942318+Heizenburger@users.noreply.github.com>
2024-11-21 01:38:54 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import ChessMentorHome from './ChessMentorHome';
+import ChessExplorer from './ChessExplorer';
 import PuzzlePage from './PuzzlePage';
+import PlayWithComputer from './PlayWithComputer';
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<ChessMentorHome />} />
+        <Route path="/explorer" component={ChessExplorer} />
         <Route path="/puzzle" element={<PuzzlePage />} />
+        <Route path="/computer" element={<PlayWithComputer />} />
       </Routes>
     </Router>
   );
